7ba13631ebcbce783cf7f2461eeeb7f5644e1f96
Ritvik sharma <ritvik.sharma@student.sl.on.ca>
2018-04-16 00:56:03 -0400

    Folder created and contact us page is done

---
+++ Header/head.js
@@ -0,0 +1,9 @@
+function search() {
+   window.open("https://www.google.com" );
+   };
+   function login() {
+	   alert("Oppps..! Connection Problem.");
+   };
+   function locate() {
+	   window.open("https://www.google.ca/maps/place/Ontario/@48.9552626,-93.7065152,5z/data=!3m1!4b1!4m5!3m4!1s0x4cce05b25f5113af:0x70f8425629621e09!8m2!3d51.253775!4d-85.323214");
+   };
